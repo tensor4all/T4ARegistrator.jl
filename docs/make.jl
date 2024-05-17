0a1074c8e3cca060fe66dbfc7ae34ad059a2dadf
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-05-17 21:34:38 +0900

    Add Documenter and LiveServer dependencies, create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using T4ARegistrator
+using T4ARegistrator.PkgBump
+using Documenter
+
+DocMeta.setdocmeta!(T4ARegistrator, :DocTestSetup, :(using T4ARegistrator); recursive=true)
+DocMeta.setdocmeta!(PkgBump, :DocTestSetup, :(using PkgBump); recursive=true)
+
+makedocs(;
+    modules=[T4ARegistrator],
+    authors="tensor4all group and contributors",
+    sitename="T4ARegistrator.jl",
+    format=Documenter.HTML(;
+        canonical="https://tensor4all.github.io/T4ARegistrator.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/tensor4all/T4ARegistrator.jl",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+```@meta
+CurrentModule = T4ARegistrator
+```
+
+# T4ARegistrator
+
+Documentation for [T4ARegistrator](https://github.com/terasakisatoshi/T4ARegistrator.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [T4ARegistrator, T4ARegistrator.PkgBump]
+```
+
