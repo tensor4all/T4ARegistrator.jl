3e14eb71e3c4380096fbd1898c8ffff6a68b782a
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-09-07 16:28:09 +0900

    Update register

--- src/T4ARegistrator.jl
+++ src/T4ARegistrator.jl
@@ -4,6 +4,7 @@ using LibGit2
 using TOML: TOML
 
 using LocalRegistry: LocalRegistry, find_package_path
+using LocalRegistry.RegistryTools: RegistryTools
 
 export bumppatch, bumpminor, bumpmajor
 include("PkgBump/PkgBump.jl")
@@ -29,37 +30,59 @@ function register(package::Union{Module,Nothing} = nothing)
                                                   """)
 
     d = Dict()
+    local pkg
     for project_file in Base.project_names
         p = joinpath(package_dir, project_file)
+        pkg = RegistryTools.Project(p)
         if isfile(p)
             d = TOML.parsefile(p)
+            break
         end
     end
 
     name = get(d, "name", nothing)
     version = get(d, "version", nothing)
-    if isnothing(name)
+    if isnothing(pkg.name)
         error("$(package) does not have a Project.toml or JuliaProject.toml file")
     end
-    if isnothing(version)
+    if isnothing(pkg.version)
         error("$(package) is not a valid package (no version)")
     end
     v = VersionNumber(version)
     branch = "register-$(name)-$(v)"
-    LocalRegistry.register(
+    registry = "git@github.com:tensor4all/T4ARegistry.git"
+
+    result = LocalRegistry.register(
         package_dir;
-        registry = "git@github.com:tensor4all/T4ARegistry.git",
+        registry = registry,
         branch = branch,
         commit = true,
         push = true,
     )
 
+    registry_path = LocalRegistry.find_registry_path(registry, pkg)
+    gitconfig = Dict()
+    registry_path, _ = LocalRegistry.check_git_registry(registry_path, gitconfig)
+    new_package = !LocalRegistry.has_package(registry_path, pkg)
+    if new_package
+        package_repo = LocalRegistry.get_remote_repo(package_path, gitconfig)
+    else
+        package_repo = ""
+    end
+
     @info "Hint: you can create a new pull request to GitHub repository via GitHub CLI:"
-    basebranch =
-        read(`git -C $(package_dir) rev-parse --abbrev-ref origin/HEAD`, String) |> chomp |> basename
-    @info "gh pr create --repo tensor4all/T4ARegistry --base $(basebranch) --head $(branch)"
+    basebranch = readchomp(`git -C . rev-parse --abbrev-ref HEAD`) # e.g., main or master
+
+    pr_title = "$(LocalRegistry.commit_title(pkg, new_package))"
+
+    tree_hash, _, _ = LocalRegistry.get_tree_hash(package_dir, gitconfig)
+
+    pr_body = "UUID: $(pkg.uuid)\nRepo: $(package_repo)\nTree: $(string(tree_hash))"
+    command = "gh pr create --repo tensor4all/T4ARegistry --base $(basebranch) --head $(branch) --title \"$(pr_title)\" --body \"$(pr_body)\""
+    println(command)
     @info "Hint: you can merge the pull request via GitHub CLI:"
-    @info "gh pr merge --repo tensor4all/T4ARegistry $(branch) --merge --auto --delete-branch"
+    println("gh pr merge --repo tensor4all/T4ARegistry $(branch) --merge --auto --delete-branch")
+    return true
 end
 
 end
